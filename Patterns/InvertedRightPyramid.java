6eb0ecb288157b7b51a5076ca6a38f073d41f04d
Monika-Thiyagarajan <monika.anj202@gmail.com>
2026-04-12 22:10:23 +0530

    SevenStarPyramid

--- Patterns/InvertedRightPyramid.java
+++ Patterns/InvertedRightPyramid.java
@@ -17,14 +17,31 @@ public class InvertedRightPyramid{
             System.out.println();
         }
     }
+    //TC : O(n^2) SC : O(1) Loop count = last value − first value + 1
+    static void sevenStarPyramid(int n){
+        for(int i=0; i<n; i++){
+            //= (n-2-i) - 0 + 1 = n - 1 - i
+            for(int j=0; j<=n-2-i; j++){
+                System.out.print(" ");
+            }
+            /*= (n+i-1) - (n-1-i) + 1
+                = 2*i + 1*/
+            for(int k=n-1-i; k<n+i; k++){
+                System.out.print("*");
+            }   
+            System.out.println("");     
+        }
+
+    }
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the size of pyramid:");
         int n = sc.nextInt();
         System.out.println("Star");
         sc.close();
-        patternStar(n);
-        System.out.println("Number: ");
-        patternNumber(n);
+        // patternStar(n);
+        // System.out.println("Number: ");
+        // patternNumber(n);
+        sevenStarPyramid(n);
     }
 }
